b02f254395bd03e4f7ebe5dfcc4a19ae5746a341
jokay03J <magrojoan3@gmail.com>
2023-03-12 00:03:50 +0100

    update routes

--- start/routes/post.ts
+++ start/routes/post.ts
@@ -1,6 +1,10 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.get('post', 'PostsController.index')
-  Route.put('post', 'PostsController.update')
+  Route.get('/', 'PostsController.index').as('posts.home')
+  Route.get('post', 'PostsController.getOne').as('posts.getOne.show')
+  Route.post('posts', 'PostsController.create').as('posts.create')
+  Route.get('createPost', 'PostsController.createShow').as('posts.create.show')
+  Route.delete('post', 'PostsController.destroy').as('posts.destroy')
+  Route.put('post', 'PostsController.update').as('posts.update')
 })
--- start/routes/user.ts
+++ start/routes/user.ts
@@ -6,4 +6,11 @@ Route.group(() => {
   Route.get('register', 'AuthController.registerShow').as('auth.register.show')
   Route.post('register', 'AuthController.register').as('auth.register')
   Route.get('logout', 'AuthController.logout').as('auth.logout')
+  Route.post('resetPassword', 'AuthController.resetPassword').as('auth.resetPassword')
+  Route.get('resetPassword/success', 'AuthController.resetPasswordSuccessShow').as(
+    'auth.resetPassword.success'
+  )
+  Route.get('resetPassword/error', 'AuthController.resetPasswordErrorShow').as(
+    'auth.resetPassword.error'
+  )
 })
